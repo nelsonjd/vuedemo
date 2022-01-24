b03934ef7308b8503e59ffd19a676f19400c08bc
Jordan D. Nelson <Jordan.doug.nelson@gmail.com>
2022-01-23 22:07:54 -0600

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,68 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h1>{{ title }}</h1>
+    <template v-for="branch in branches">
+      <input
+        type="radio"
+        :id="branch"
+        :key="branch"
+        :value="branch"
+        name="branch"
+        v-model="currentBranch"
+      />
+      <label :key="branch.sha" for="branch">{{ branch }}</label>
+    </template>
+    <p>IBM/AMLSim@{{ currentBranch }}</p>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="commit in commits"
+      :key="commit.sha">
+        <a :href="commit.html_url">{{ commit.sha | trucate }}</a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    title: String,
+  },
+  created: function () {
+    this.fetchData();
+  },
+  data: function () {
+    const branches = ["master", "new-schema"];
+    return {
+      branches: branches,
+      commits: [],
+      currentBranch: branches[0],
+    };
+  },
+  watch: {
+    currentBranch: "fetchData",
+  },
+  filters: {
+    trucate: function(v) {
+      return v.slice(0, 7)
+    }
+  },
+  methods: {
+    fetchData: function () {
+      fetch(this.apiUrl())
+        .then((response) => response.json())
+        .then((data) => {
+          this.commits = data;
+        });
+    },
+    apiUrl: function () {
+      return (
+        "https://api.github.com/repos/ibm/amlsim/commits?per_page=3&sha=" +
+        this.currentBranch
+      );
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -49,7 +75,6 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
